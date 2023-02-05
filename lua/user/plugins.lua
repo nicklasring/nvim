d788d0561d0e4f78eba87b72c81f08e750933623
Nicklas Ring <nicklas@verendus.se>
2023-02-05 15:21:21 +0100

    New colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -58,7 +58,7 @@ return packer.startup(function(use)
 	use {"folke/which-key.nvim"}
 
 	-- Colorschemes
-  use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
+  use { "Mofiqul/vscode.nvim", commit = "f2e77967f78bd215550ae73d84f530c516f2606f" }
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
 
 	-- Cmp 
